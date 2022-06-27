17b04e81cbd480375b7691c2ba982c232a3cf0fe
Kshitij Baluni <kbaluni@auracompany.com>
2022-06-27 16:13:14 +0530

    intermediate stream operations code.

--- core-java-module/src/main/java/CoreJavaStream.java
+++ core-java-module/src/main/java/CoreJavaStream.java
@@ -10,15 +10,48 @@ import java.util.stream.Collectors;
  */
 public class CoreJavaStream {
 
-  private void intermediateStreamOperations() {
-    //1. map - Sample code to test map intermediate operations
-    List<Integer> list = Arrays.asList(2, 4, 3, 5, 3, 4, 2);
-    List result = list.stream().map(x -> x * x).collect(Collectors.toList());
-    System.out.println("Map result list - " + result);
+  /**
+   * map - Sample code to test map intermediate operations.
+   *
+   * @param list
+   * @return result List
+   */
+  private List mapStreamIntermediateOperations(List<Integer> list) {
+    return list.stream().map(x -> x * x).collect(Collectors.toList());
+  }
+
+  /**
+   * filter - intermediate operations stream based on the predicate(functional interface/filter condition).
+   *
+   * @param list
+   * @return result List
+   */
+  private List filterStreamIntermediateOperations(List<Integer> list) {
+    return list.stream().filter(x -> (x % 2 == 0)).collect(Collectors.toList());
+  }
+
+  /**
+   * sorted - The sorted method is used to sort the stream.
+   *
+   * @param list
+   * @return result List
+   */
+  private List sortedStreamIntermediateOperations(List<Integer> list) {
+    return list.stream().sorted().collect(Collectors.toList());
   }
 
   public static void main(String[] args) {
     CoreJavaStream coreJavaStream = new CoreJavaStream();
-    coreJavaStream.intermediateStreamOperations();
+    List<Integer> list = Arrays.asList(2, 4, 3, 5, 3, 4, 2);
+    List result = null;
+
+    result = coreJavaStream.mapStreamIntermediateOperations(list);
+    System.out.println("Map IO stream :" + result);
+
+    result = coreJavaStream.filterStreamIntermediateOperations(list);
+    System.out.println("Filter IO stream :" + result);
+
+    result = coreJavaStream.sortedStreamIntermediateOperations(list);
+    System.out.println("Sorted IO stream:" + result);
   }
 }
