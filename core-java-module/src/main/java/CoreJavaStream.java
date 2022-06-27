64172a16d025cc2c8d34198316e2dd160d6a5bc6
Kshitij Baluni <kbaluni@auracompany.com>
2022-06-27 16:34:33 +0530

    update the output for each stream example.

--- core-java-module/src/main/java/CoreJavaStream.java
+++ core-java-module/src/main/java/CoreJavaStream.java
@@ -40,18 +40,57 @@ public class CoreJavaStream {
     return list.stream().sorted().collect(Collectors.toList());
   }
 
+  /**
+   * collect - The collect method is used to return the result of the intermediate operations performed on the stream.
+   *
+   * @param list
+   * @return result List
+   */
+  private List collectStreamTerminateOperation(List<Integer> list) {
+    return list.parallelStream().map(x-> x*2).collect(Collectors.toList());
+  }
+
+  /**
+   * forEach - The forEach method is used to iterate through every element of the stream.
+   * @param list
+   */
+  private void forEachStreamTerminateOperation(List<Integer> list) {
+    list.stream().forEach(x -> System.out.print(String.format(" %s",x)));
+  }
+
+  /**
+   * reduce -The reduce method is used to reduce the elements of a stream to a single value.
+   * The reduce method takes a BinaryOperator as a parameter.
+   *
+   * @param list
+   * @return
+   */
+  private int reduceStreamTerminateOperation(List<Integer> list) {
+    return list.stream().limit(3).reduce(0, (ans, i) -> ans + i);
+  }
+
+
   public static void main(String[] args) {
     CoreJavaStream coreJavaStream = new CoreJavaStream();
     List<Integer> list = Arrays.asList(2, 4, 3, 5, 3, 4, 2);
     List result = null;
 
     result = coreJavaStream.mapStreamIntermediateOperations(list);
-    System.out.println("Map IO stream :" + result);
+    System.out.println("\nMap IO stream: " + result);
 
     result = coreJavaStream.filterStreamIntermediateOperations(list);
-    System.out.println("Filter IO stream :" + result);
+    System.out.println("\nFilter IO stream: " + result);
 
     result = coreJavaStream.sortedStreamIntermediateOperations(list);
-    System.out.println("Sorted IO stream:" + result);
+    System.out.println("\nSorted IO stream: " + result);
+
+    result = coreJavaStream.collectStreamTerminateOperation(list);
+    System.out.println("\nCollect TO stream: " + result);
+
+    System.out.print("\nForEach TO stream: ");
+    coreJavaStream.forEachStreamTerminateOperation(list);
+
+    int output = coreJavaStream.reduceStreamTerminateOperation(list);
+    System.out.println("\n\nReduce TO stream: " + output);
   }
 }
