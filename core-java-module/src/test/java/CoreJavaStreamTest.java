3e96c98283a55685413c8aa25d9545e07539bf11
Kshitij Baluni <kbaluni@auracompany.com>
2022-06-29 14:53:58 +0530

    removed the in used import.

--- core-java-module/src/test/java/CoreJavaStreamTest.java
+++ core-java-module/src/test/java/CoreJavaStreamTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
