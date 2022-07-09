e95abd3d5795eb84a752bb57b2e08240e3f4baa3
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-10 00:47:51 +0530

    added test for CoreJavaException

--- core-java-module/src/main/java/exception/CoreJavaException.java
+++ core-java-module/src/main/java/exception/CoreJavaException.java
@@ -1,5 +1,8 @@
 package exception;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Core Java Exception Demo.
  *
@@ -7,4 +10,43 @@ package exception;
  * @since 9 July 2022
  */
 public class CoreJavaException {
+  private final static Logger logger = Logger.getLogger(CoreJavaException.class.getName());
+
+  /**
+   * Demo for Null Pointer Exception.
+   * Unchecked Exception.
+   */
+  public static void nullPointerException() {
+    try {
+      String testNull = null;
+      testNull.toString();
+    } catch (NullPointerException e) {
+      logger.log(Level.INFO, e.getStackTrace().toString());
+    }
+  }
+
+  /***
+   * Demo for Arithmetic Exception.
+   * Unchecked Exception.
+   */
+  public static void arithmeticException() {
+    try {
+      int a = 5 / 0;
+    } catch (ArithmeticException e) {
+      logger.log(Level.INFO, e.getStackTrace().toString());
+    }
+  }
+
+  /***
+   * Demo for ArrayIndexOutOfBoundException.
+   * Unchecked Exception.
+   */
+  public static void arrayIndexOutOfBoundException() {
+    try {
+      int a[] = {1, 2, 3};
+      a[3] = 1;
+    } catch (ArrayIndexOutOfBoundsException e) {
+      logger.log(Level.INFO, e.getStackTrace().toString());
+    }
+  }
 }
---
+++ core-java-module/src/test/java/exception/CoreJavaExceptionTest.java
@@ -0,0 +1,13 @@
+package exception;
+
+
+import org.junit.Test;
+
+public class CoreJavaExceptionTest {
+  @Test
+  public void testUncheckedExceptions() {
+    CoreJavaException.nullPointerException();
+    CoreJavaException.arithmeticException();
+    CoreJavaException.arrayIndexOutOfBoundException();
+  }
+}
