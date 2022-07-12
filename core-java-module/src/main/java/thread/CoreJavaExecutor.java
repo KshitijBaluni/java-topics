9d0f5374238a36b2a8c954761e1ca44edd1fc06f
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-12 15:45:04 +0530

    Executor class implementation.

--- core-java-module/src/main/java/thread/CoreJavaExecutor.java
+++ core-java-module/src/main/java/thread/CoreJavaExecutor.java
@@ -1,4 +1,27 @@
 package thread;
 
-public class CoreJavaExecutor {
+import java.util.concurrent.Callable;
+
+/***
+ * Demo for Executor
+ *
+ * @author kshitijbaluni
+ * @since 12 July 2022
+ */
+public class CoreJavaExecutor implements Callable<String> {
+    private String coreJavaExecutorTask;
+
+    /**
+     * Constructor CoreJavaExecutor
+     *
+     * @param coreJavaExecutorTask
+     */
+    public CoreJavaExecutor(String coreJavaExecutorTask) {
+        this.coreJavaExecutorTask = coreJavaExecutorTask;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return String.format("%s@%s", coreJavaExecutorTask, coreJavaExecutorTask.hashCode());
+    }
 }
