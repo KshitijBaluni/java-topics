c9175c039335fbdea214d49ca9a0d03e64af5764
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-14 00:12:36 +0530

    added CoreJavaSingleton class.

--- core-java-module/src/main/java/designpatterns/creational/singleton/CoreJavaSingleton.java
+++ core-java-module/src/main/java/designpatterns/creational/singleton/CoreJavaSingleton.java
@@ -1,12 +1,37 @@
 package designpatterns.creational.singleton;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/***
+ * Demo for Singleton Design Pattern.
+ *
+ * @author kshitijbaluni
+ * @since 14 July 2022
+ */
 public class CoreJavaSingleton {
   private static CoreJavaSingleton coreJavaSingleton = new CoreJavaSingleton();
+  private static final Logger logger = LogManager.getLogManager().getLogger(CoreJavaSingleton.class.getName());
 
+  /**
+   * Private constructor
+   */
   private CoreJavaSingleton() {
   }
 
+  /**
+   * Get singleton object.
+   *
+   * @return CoreJavaSingleton object
+   */
   public static CoreJavaSingleton getCoreJavaSingleton() {
     return coreJavaSingleton;
+  }
+
+  /**
+   * Simple method to log the hashcode of the singleton object.
+   */
+  public void simpleMethod() {
+    logger.info("Hashcode of singleton object -" + coreJavaSingleton);
   }
 }
