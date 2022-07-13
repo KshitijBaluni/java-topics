a399c8e6abb4b07da22c935413eb26892577da5d
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-13 23:54:48 +0530

    added initial singleton

--- core-java-module/src/main/java/designpatterns/creational/singleton/CoreJavaSingleton.java
+++ core-java-module/src/main/java/designpatterns/creational/singleton/CoreJavaSingleton.java
@@ -1,4 +1,12 @@
 package designpatterns.creational.singleton;
 
 public class CoreJavaSingleton {
+  private static CoreJavaSingleton coreJavaSingleton = new CoreJavaSingleton();
+
+  private CoreJavaSingleton() {
+  }
+
+  public static CoreJavaSingleton getCoreJavaSingleton() {
+    return coreJavaSingleton;
+  }
 }
