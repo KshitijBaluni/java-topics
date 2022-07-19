dccae13edd2c8e94e4c32aa4734fdfa184fc5a37
Kshitij Baluni <kbaluni@auracompany.com>
2022-07-19 18:54:03 +0530

    added java doc.

--- core-java-module/src/main/java/serialization/JsonPropertyAnnotation.java
+++ core-java-module/src/main/java/serialization/JsonPropertyAnnotation.java
@@ -5,24 +5,49 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Demo class for Json Property Demonstration.
+ *
+ * @author kshitijbaluni
+ * @since 19 July 2022
+ */
 public class JsonPropertyAnnotation {
   private ObjectMapper objectMapper;
   private Gson gsonObject;
 
+  /**
+   * Constructor for setting Object Mapper and Gson.
+   *
+   * @param objectMapper object mapper object
+   * @param gsonObject gson object
+   */
   public JsonPropertyAnnotation(ObjectMapper objectMapper, Gson gsonObject) {
     this.objectMapper = objectMapper;
     this.gsonObject = gsonObject;
   }
 
+  /**
+   * Get Object Mapper.
+   *
+   * @return Object Mapper
+   */
   public ObjectMapper getObjectMapper() {
     return objectMapper;
   }
 
+  /**
+   * Get Gson object.
+   *
+   * @return gson object.
+   */
   public Gson getGsonObject() {
     return gsonObject;
   }
 }
 
+/**
+ * DTO User class.
+ */
 class User {
   @SerializedName("first-name")
   public String firstName;
@@ -33,26 +58,53 @@ class User {
   @JsonProperty("home_phone")
   public String workPhone;
 
+  /**
+   * Parameterized constructor.
+   *
+   * @param firstName first name
+   * @param lastName last name
+   * @param mobilePhone mobile no
+   * @param workPhone work phone
+   */
   public User(String firstName, String lastName, String mobilePhone, String workPhone) {
-    super();
     this.firstName = firstName;
     this.lastName = lastName;
     this.mobilePhone = mobilePhone;
     this.workPhone = workPhone;
   }
 
+  /**
+   * Get first name.
+   *
+   * @return first name string.
+   */
   public String getFirstName() {
     return firstName;
   }
 
+  /**
+   * Get last name.
+   *
+   * @return last name string
+   */
   public String getLastName() {
     return lastName;
   }
 
+  /**
+   * Get Mobile Phone.
+   *
+   * @return mobile phone as string
+   */
   public String getMobilePhone() {
     return mobilePhone;
   }
 
+  /**
+   * Get Work Phone.
+   *
+   * @return work phone as string
+   */
   public String getWorkPhone() {
     return workPhone;
   }
