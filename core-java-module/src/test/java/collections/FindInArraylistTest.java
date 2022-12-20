8759b1e92a91afa8a5d83426b8304f8b151e20c7
ankitrawat <ankit.rwt93@gmail.com>
2022-12-20 15:05:29 +0530

    Pr comments resolved arraylist

--- core-java-module/src/test/java/collections/FindInArraylistTest.java
+++ core-java-module/src/test/java/collections/FindInArraylistTest.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertTrue;
 
 public class FindInArraylistTest {
+
+
     @Test
     public void arrayFind() {
         ArrayList<Integer> fia
@@ -18,6 +20,46 @@ public class FindInArraylistTest {
         fia.add(23);
         FindInArraylist findInArraylist = new FindInArraylist(fia, 23);
         boolean result = findInArraylist.arrayFind();
+        assertTrue(result);
+    }
+
+    @Test
+    public void arrayFindParamFunction() {
+        ArrayList<Integer> fia
+                = new ArrayList<Integer>();
+        fia.add(2);
+        fia.add(4);
+        fia.add(7);
+        fia.add(9);
+        fia.add(23);
+        FindInArraylist findInArraylist = new FindInArraylist(fia, 23);
+        boolean result = findInArraylist.arrayFind(fia,23);
+        assertTrue(result);
+    }
+    @Test
+    public void arrayFindForEach() {
+        ArrayList<Integer> fia
+                = new ArrayList<Integer>();
+        fia.add(2);
+        fia.add(4);
+        fia.add(7);
+        fia.add(9);
+        fia.add(23);
+        FindInArraylist findInArraylist = new FindInArraylist(fia, 23);
+        boolean result =findInArraylist.findArrayForEach();
+        assertTrue(result);
+    }
+    @Test
+    public void arrayFindForEachParamFunc() {
+        ArrayList<Integer> fia
+                = new ArrayList<Integer>();
+        fia.add(2);
+        fia.add(4);
+        fia.add(7);
+        fia.add(9);
+        fia.add(23);
+        FindInArraylist findInArraylist = new FindInArraylist(fia, 23);
+        boolean result =findInArraylist.findArrayForEach(fia,23);
         assertTrue(result);
     }
 }
--- core-java-module/src/test/java/collections/ReverseArraylistTest.java
+++ core-java-module/src/test/java/collections/ReverseArraylistTest.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ReverseArraylistTest {
     @Test
-    public void reverseArraylist() {
+    public void arrayListUsingIntegerArray() {
         ReverseArraylist reverseArraylist = new ReverseArraylist();
         ArrayList<Integer> fia
                 = new ArrayList<Integer>();
@@ -20,5 +21,19 @@ public class ReverseArraylistTest {
         fia.add(23);
         ArrayList<Integer> arr = reverseArraylist.arrayListUsingIntegerArray(fia);
         assertEquals(Arrays.asList(23, 9, 7, 4, 2), arr);
+    }
+    @Test
+    public void reverseArraylist(){
+        ReverseArraylist reverseArraylist = new ReverseArraylist();
+        ArrayList<Integer> fia
+                = new ArrayList<Integer>();
+        fia.add(2);
+        fia.add(4);
+        fia.add(7);
+        fia.add(9);
+        fia.add(23);
+        ArrayList<Integer> arr=reverseArraylist.reverseArraylist(fia);
+        assertEquals(Arrays.asList(23, 9, 7, 4, 2), arr);
+        assertNotEquals(Arrays.asList(2,4,7,9,23), arr);
     }
 }
