a223446f8e58714d06270e969804957ff3e12315
ankitrawat <ankit.rwt93@gmail.com>
2022-12-22 13:57:51 +0530

    gson examples

--- src/main/java/gson/Employee.java
+++ src/main/java/gson/Employee.java
@@ -1,27 +1,71 @@
 package gson;
+/**
+ * gson
+ *
+ * @author Ankit Rawat
+ * @since 21 Dec 2022
+ */
 public class Employee {
+
     private String name;
     private int salary;
     private int totalYearsInComp;
+
+    /**
+     * Return employee's name.
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Set employee's name
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * Return Employee's salary.
+     *
+     * @return name
+     */
     public int getSalary() {
         return salary;
     }
+
+    /**
+     * Set employee's salary
+     *
+     * @param salary
+     */
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    /**
+     * Return Employee's Total years in company.
+     *
+     * @return totalYearsInComp
+     */
     public int getTotalYearsInComp() {
         return totalYearsInComp;
     }
 
+    /**
+     * Set employee's Total years in company
+     *
+     * @param totalYearsInComp
+     */
     public void setTotalYearsInComp(int totalYearsInComp) {
         this.totalYearsInComp = totalYearsInComp;
     }
+
+
     public String toString()
     {
         return "Employee [name=" + name+ ", Total years in company=" + totalYearsInComp + ", salary=" + salary + "]";
--- src/test/java/gson/EmployeeTest.java
+++ src/test/java/gson/EmployeeTest.java
@@ -3,8 +3,6 @@ package gson;
 import com.google.gson.Gson;
 import org.junit.Test;
 
-import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
 
 public class EmployeeTest {
@@ -17,16 +15,8 @@ public class EmployeeTest {
         employee.setSalary(150);
         employee.setTotalYearsInComp(2);
         String employeeJson = gson.toJson(employee);
-        System.out.println(employeeJson);
         assertEquals("{\"name\":\"Ankit\",\"salary\":150,\"totalYearsInComp\":2}",employeeJson);
-
-        Employee empGenerated = gson.fromJson(
-                gson.toJson(employee), Employee.class);
-        System.out.println(empGenerated);
+        Employee empGenerated = gson.fromJson(gson.toJson(employee), Employee.class);
         assertEquals("Employee [name=Ankit, Total years in company=2, salary=150]", empGenerated);
-
-
-
-
     }
 }
