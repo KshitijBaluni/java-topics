022550c2740b6594898e3ce9011d11d656067d25
ankitrawat <ankit.rwt93@gmail.com>
2022-12-22 13:46:48 +0530

    gson examples

--- src/test/java/gson/EmployeeTest.java
+++ src/test/java/gson/EmployeeTest.java
@@ -3,6 +3,10 @@ package gson;
 import com.google.gson.Gson;
 import org.junit.Test;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 public class EmployeeTest {
 
     @Test
@@ -14,5 +18,15 @@ public class EmployeeTest {
         employee.setTotalYearsInComp(2);
         String employeeJson = gson.toJson(employee);
         System.out.println(employeeJson);
+        assertEquals("{\"name\":\"Ankit\",\"salary\":150,\"totalYearsInComp\":2}",employeeJson);
+
+        Employee empGenerated = gson.fromJson(
+                gson.toJson(employee), Employee.class);
+        System.out.println(empGenerated);
+        assertEquals("Employee [name=Ankit, Total years in company=2, salary=150]", empGenerated);
+
+
+
+
     }
 }
