e9a156b366f3cb920dcadbc2a606b6c0ead301b0
ankitrawat <ankit.rwt93@gmail.com>
2022-12-30 14:53:41 +0530

    gson into file test pr comments fix

--- src/main/java/gson/CreateFile.java
+++ src/main/java/gson/CreateFile.java
@@ -2,8 +2,20 @@ package gson;
 
 import java.io.*;
 
+/**
+ * gson
+ *
+ * @author Ankit Rawat
+ * @since 22 Dec 2022
+ */
 public class CreateFile {
 
+    /**
+     * Write into output.txt file
+     *
+     * @param str-String to write in file
+     * @return name
+     */
     public void writeFile(String str) throws IOException {
         int ch;
 
@@ -13,20 +25,23 @@ public class CreateFile {
         fw.close();
     }
 
+    /**
+     * Read file from output.txt file
+     *
+     * @return sb.toString string form of stringbuilder reference
+     */
+    public String readFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("output.txt"));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-        public String readFile() throws IOException {
-            BufferedReader br = new BufferedReader(new FileReader("output.txt"));
-            try {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
-
-                while (line != null) {
-                    sb.append(line);
-                    line = br.readLine();
-                }
-                return sb.toString();
-            } finally {
-                br.close();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
             }
+            return sb.toString();
+        } finally {
+            br.close();
         }
-    }
+    }}
