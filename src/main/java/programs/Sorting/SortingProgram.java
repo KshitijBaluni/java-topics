48a28e839cef0b9e7f6f137d2b51892bdf1db032
ankitrawat <ankit.rwt93@gmail.com>
2022-12-30 16:03:34 +0530

    sorting programs

--- src/main/java/programs/Sorting/SortingProgram.java
+++ src/main/java/programs/Sorting/SortingProgram.java
@@ -2,7 +2,20 @@ package programs.Sorting;
 
 import java.util.Arrays;
 
+/**
+ * Sorting
+ *
+ * @author Ankit Rawat
+ * @since 30 Dec 2022
+ */
 public class SortingProgram {
+
+    /**
+     * Sort using for loop
+     *
+     * @param arr-array to sort
+     * @return arr- sorted array
+     */
     public int[] loopSort(int[]arr){
         for (int i=0; i<arr.length;i++){
             for (int j=i+1;j< arr.length;j++){
@@ -17,6 +30,12 @@ public class SortingProgram {
         return arr;
     }
 
+    /**
+     * Sort using sort() method
+     *
+     * @param arr-array to sort
+     * @return arr-sorted array
+     */
     public int[] arrSort(int []arr){
         Arrays.sort(arr);
         return arr;
