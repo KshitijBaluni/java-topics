d5fdb465cef063c9970e286a4b3d7c96d8c55a7f
ankitrawat <ankit.rwt93@gmail.com>
2023-01-09 14:34:20 +0530

    Binary search class name starts with capital fix

--- src/test/java/programs/string/BinarySearchintTest.java
+++ src/test/java/programs/string/BinarySearchintTest.java
@@ -5,12 +5,12 @@ import programs.BinarySearchInt;
 
 import static org.junit.Assert.assertEquals;
 
-public class binarySearchIntTest {
+public class BinarySearchintTest {
     @Test
     public void binarySearchIntArr(){
         int[] arr={4,6,12,15,18,25,34};
-        BinarySearchInt binarySearchInt=new BinarySearchInt();
-        int index=binarySearchInt.binarySearchIntArr(arr,0,arr.length-1,18);
+        BinarySearchInt binarySearchint=new BinarySearchInt();
+        int index=binarySearchint.binarySearchIntArr(arr,0,arr.length-1,18);
         assertEquals(4, index);
     }
 }
